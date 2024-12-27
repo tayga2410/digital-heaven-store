a5329ecdce95126e0eb43ad52407bb872359db77
tayga2410 <moldazhanov.eldar@gmail.com>
2024-12-27 16:58:16 +0500

    feat: addedd payment faq after cart payments

---
+++ frontend/src/app/payment/page.tsx
@@ -0,0 +1,11 @@
+export default function Payments() {
+    return (
+        <div className="information container">
+            <h2 className="information__title">Информация об оплате</h2>
+            <p>
+             На данный момент, мы подключаем платежные системы. Пожалуйста, попробуйте позднее
+            </p>
+
+        </div>
+    )
+}
