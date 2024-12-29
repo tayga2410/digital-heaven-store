60b662067ea6e4f0559e0903d620b3bcc1871c1c
tayga2410 <moldazhanov.eldar@gmail.com>
2024-12-29 20:16:56 +0500

    fix: moved link into logo span

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -56,9 +56,9 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header__container">
-      <Link href="/">
-        <span className="header__logo">Digital Heaven</span>
-        </Link>
+      
+        <span className="header__logo"><Link href="/">Digital Heaven</Link></span>
+        
         <button
           className="header__search-icon"
           onClick={() => setIsSearchOpen(!isSearchOpen)}
